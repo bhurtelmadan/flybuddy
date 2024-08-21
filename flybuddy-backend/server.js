442280bbece7d92a502cd56ab1b67bffb6f24773
Madan Bhurtel <madan.bhurtel@toshibagcs.com>
2024-08-21 01:41:46 -0500

    server.js

--- flybuddy-backend/server.js
+++ flybuddy-backend/server.js
@@ -9,7 +9,6 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
